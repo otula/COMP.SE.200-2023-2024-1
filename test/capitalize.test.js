ed6c4096b2977aa2db467276344160693d16d38e
RECORDON ELISA <elisa.recordon@insa-rouen.fr>
2023-12-04 00:48:42 +0200

    style: fix indentation

--- test/capitalize.test.js
+++ test/capitalize.test.js
@@ -2,23 +2,23 @@ import capitalize from "../src/capitalize.js";
 
 describe('capitalize', function() {
     it('should turn the first letter of a lowercase string to uppercase', function() {
-	expect(capitalize("example")).toBe("Example");
+        expect(capitalize("example")).toBe("Example");
     });
     
     it('should capitalize undefined value as a string', function() {
-	expect(capitalize(undefined)).toBe("Undefined");
+        expect(capitalize(undefined)).toBe("Undefined");
     });
     
     it('should turn all uppercase letters to lowercase except the first one', function() {
-	expect(capitalize("FRED")).toBe("Fred");
+        expect(capitalize("FRED")).toBe("Fred");
     });
     
     it('should capitalize lowercase strings despite them containing a special mark', function() {
-	expect(capitalize("exam©le")).toBe("Exam©le");
+        expect(capitalize("exam©le")).toBe("Exam©le");
     });
     
     it('should turn numbers into a string', function() {
-	expect(capitalize(34)).toBe("34");
+        expect(capitalize(34)).toBe("34");
     });
     
     it('should leave it as it is when given an empty string', function() {
@@ -26,10 +26,10 @@ describe('capitalize', function() {
     });
     
     it('should capitalize the first element of an array and use , as a separator', function() {
-	expect(capitalize(["fiRST", "SECOND", "ThirD"])).toBe("First,second,third");
+        expect(capitalize(["fiRST", "SECOND", "ThirD"])).toBe("First,second,third");
     });
 
     it('should use the usual notation for objects', function() {
-	expect(capitalize({"a": 1, "b": 2, 3: null})).toBe("[object object]");
+        expect(capitalize({"a": 1, "b": 2, 3: null})).toBe("[object object]");
     });
 });
