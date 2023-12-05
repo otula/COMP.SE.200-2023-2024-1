0defbb42578c35599e5b67de4256bde38efbfa01
OGgrizzley <owengossen@hotmail.com>
2023-12-05 18:22:01 +0200

    Test Coveralls integration

--- src/add.js
+++ src/add.js
@@ -16,3 +16,4 @@ import createMathOperation from './.internal/createMathOperation.js'
 const add = createMathOperation((augend, addend) => augend + addend, 0)
 
 export default add
+console.log("Hello, Coveralls!");
