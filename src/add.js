9218c32b007da130e0aeef986dd852d4fa256d95
OGgrizzley <owengossen@hotmail.com>
2023-12-05 23:30:51 +0200

    Your commit message here

--- src/add.js
+++ src/add.js
@@ -16,4 +16,3 @@ import createMathOperation from './.internal/createMathOperation.js'
 const add = createMathOperation((augend, addend) => augend + addend, 0)
 
 export default add
-console.log("Hello, Coveralls!");
