fb501222bfdbf33c457457eda36746eb32a23d6f
Eetu Pulkkinen <47150327+eetulauri@users.noreply.github.com>
2023-12-10 19:01:59 +0200

    Remove unnecessary test cases for toString function

--- test/unit/words.test.js
+++ test/unit/words.test.js
@@ -18,15 +18,4 @@ describe('words', () => {
         const string = '';
         expect(words(string)).toEqual([]);
     });
-
-    it('should return an array of words that match the pattern when the input is a string with special characters', () => {
-        const string = 'The_quick-brown.fox jumps_over the_lazy dog';
-        const pattern = /\w+/g;
-        expect(words(string, pattern)).toEqual(['The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog']);
-    });
-
-    it('should return an array of words when the input is a string with unicode characters', () => {
-        const string = 'The quick brown 🦊 jumps over the lazy 🐶';
-        expect(words(string)).toEqual(['The', 'quick', 'brown', 'jumps', 'over', 'the', 'lazy']);
-    });
 });
